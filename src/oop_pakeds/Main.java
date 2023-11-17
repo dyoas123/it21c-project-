ae92be42da840e59497229e2de867b986e5ce9ad
dyoas123 <rautrautjoash1@gmail.com>
2023-11-17 13:57:37 +0800

    added first package

---
+++ src/oop_pakeds/Main.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package oop_pakeds;
+
+/**
+ *
+ * @author admin
+ */
+public class Main {
+    public static void main(String []args){
+        Computer computer = new Computer ("ryzen 5",5.000);
+        
+        computer.showComputerName();
+        computer.showComputerPrice();
+        
+    }
+    
+
+}
